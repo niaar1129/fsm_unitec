86c6d99711fd3c52403f510f3a5a66b3490c4e4e
arian1 <ariangr96@gmail.com>
2026-09-01 16:28:07 -0500

    Expansión de tabla de enrutamiento: Inyección de endpoint GET para fase de descarga de sincronización bidireccional

--- operaciones/urls.py
+++ operaciones/urls.py
@@ -1,13 +1,20 @@
 """
 operaciones/urls.py
+Definición topológica del perímetro de red para el módulo de operaciones.
+El prefijo 'api/sync/' es inyectado dinámicamente por el multiplexor raíz (core/urls.py).
 """
 from django.urls import path
 
-from .views import SyncTareasView
+from .views import SyncTareasView, SincronizacionDescendenteView
 
 app_name = 'operaciones'
 
 urlpatterns = [
-    # POST http://127.0.0.1:8000/api/sync/tareas/
+    # POST https://unitec-fsm-api.onrender.com/api/sync/tareas/
+    # Fase 1: Replicación Ascendente (Push) - Recepción de ráfagas desde el cliente SQLite
     path('tareas/', SyncTareasView.as_view(), name='sync-tareas'),
+    
+    # GET https://unitec-fsm-api.onrender.com/api/sync/tareas/descarga/
+    # Fase 2: Replicación Descendente (Pull) - Emisión del estado global hacia el cliente React Native
+    path('tareas/descarga/', SincronizacionDescendenteView.as_view(), name='sync_pull'),
 ]
